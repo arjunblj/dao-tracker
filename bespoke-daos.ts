1fd5df5f944854c186dd631ea09ab8a0a9d80103
Arjun Balaji <arjunblj@gmail.com>
2021-05-24 19:28:36 -0700

    feat: add support for bespoke DAOs

---
+++ bespoke-daos.ts
@@ -0,0 +1,34 @@
+export async function fetchBespokeDAOs() {
+  const daoInfo = [
+    {
+      daoName: 'Aave DAO',
+      daoAddress: ['0x25F2226B597E8F9514B3F68F00f494cF4f286491', '0x464C71f6c2F760DdA6093dCB91C24c39e5d6e18c']
+    },
+    {
+      daoName: 'Alchemix DAO',
+      daoAddress: '0x8392f6669292fa56123f71949b52d883ae57e225'
+    },
+    {
+      daoName: 'BadgerDAO',
+      daoAddress: '0x4441776e6a5d61fa024a5117bfc26b953ad1f425'
+    },
+    {
+      daoName: 'Balancer',
+      daoAddress: ['0xb618F903ad1d00d6F7b92f5b0954DcdC056fC533', '0xce88686553686DA562CE7Cea497CE749DA109f9F']
+    },
+    {
+      daoName: 'Compound',
+      daoAddress: ['0x3d9819210a31b4961b30ef54be2aed79b9c9cd3b', '0x2775b1c75658Be0F640272CCb8c72ac986009e38']
+    },
+    {
+      daoName: 'Synthetix DAO',
+      daoAddress: ['0xEb3107117FEAd7de89Cd14D463D340A2E6917769', '0x86626E1BbBd0ce95ED52e0C5E19f371a6640B591']
+    },
+    {
+      daoName: 'Uniswap',
+      daoAddress: ['0x1a9c8182c09f50c8318d769245bea52c32be35bc', '0x4750c43867ef5f89869132eccf19b9b6c4286e1a']
+    }
+  ]
+
+  return daoInfo
+}
--- index.ts
+++ index.ts
@@ -1,16 +1,21 @@
 import { fetchDAOStackDAOs } from './daostack'
-import { fetchAragonDAOs } from './aragon'
+import { fetchBespokeDAOs } from './bespoke-DAOs'
+// import { fetchAragonDAOs } from './aragon'
 import { initEthersProvider, fetchTokenList, fetchTokenBalances } from './helpers'
 
 async function main() {
   const provider = initEthersProvider()
 
   const { tokens } = await fetchTokenList()
-  const daoStackDAOs = await fetchDAOStackDAOs()
+  // const daoStackDAOs = await fetchDAOStackDAOs()
+  //
+  // const daoBalances = await fetchTokenBalances(provider, daoStackDAOs, tokens)
+  // console.log(daoBalances.filter(balance => Object.keys(balance.tokenHoldings).length > 1))
 
-  const daoBalances = await fetchTokenBalances(provider, daoStackDAOs, tokens)
+  const bespokeDAOs = await fetchBespokeDAOs()
+  const daoBalances = await fetchTokenBalances(provider, bespokeDAOs, tokens)
 
-  console.log(daoBalances.filter(balance => Object.keys(balance.tokenHoldings).length > 1))
+  console.log(daoBalances)
 }
 
 main()
